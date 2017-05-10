76eed88fbaabf45f16a15b29af250918f5a53a56
Michael <michaelp.pershtein@tufin.com>
2017-05-10 20:31:14 +0300

    integration with groovy runner

--- src/main/java/com/tufin/health/check/PluginService.java
+++ src/main/java/com/tufin/health/check/PluginService.java
@@ -4,16 +4,43 @@ package com.tufin.health.check;
 import com.tufin.health.check.groovy.GroovyRunner;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PluginService {
+
     private PluginTable pluginTable = new PluginTable();
+    private final String SUPPORTED_TYPES = ".groovy";
 
     PluginService(){
-        pluginTable.addPluginObject("test1", new PluginObject("test1", "test1.groovy"));
-        pluginTable.addPluginObject("test2", new PluginObject("test2", "test2.groovy"));
-        pluginTable.addPluginObject("test3", new PluginObject("test3", "test3.groovy"));
+
+        File pluginDirectory = new File("/usr/local/st/healthcheck/plugins");
+        File[] pluginFiles = pluginDirectory.listFiles();
+/*
+        if(pluginFiles != null) {
+            List<File> scriptFiles = Arrays.asList(pluginFiles).stream()
+                    .filter(file -> file.getName().contains(SUPPORTED_TYPES))
+                    .collect(Collectors.toList());
+*/
+        if(pluginFiles != null) {
+            List<File> scriptFiles = Arrays.asList(pluginFiles);
+
+            scriptFiles.stream()
+                    .forEach(file -> {
+//                                String pluginName = file.getName().substring(0, file.getName().lastIndexOf(SUPPORTED_TYPES));
+//                                pluginTable.addPluginObject( pluginName, new PluginObject(pluginName, file.getName()));
+
+                        String fileName = file.getName();
+                        if(fileName.contains(SUPPORTED_TYPES)) {
+                            String pluginName = fileName.substring(0, fileName.lastIndexOf(SUPPORTED_TYPES));
+                            pluginTable.addPluginObject(pluginName, new PluginObject(pluginName, fileName));
+                        }
+                    });
+        }
+
     }
 
     public List<PluginObject> getListOfAllPlugins(){
