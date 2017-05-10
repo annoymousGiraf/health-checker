a26871dbbacc6ed5d043107df7c78675825c0175
Stas <stas.obydionnov@tufin.com>
2017-05-11 02:15:04 +0300

    Cleanup check time sync script

--- src/main/resources/plugins/DATimeHelper.groovy
+++ src/main/resources/plugins/DATimeHelper.groovy
@@ -1,10 +1,5 @@
 package plugins
 
-import groovyx.net.http.RESTClient
-//import groovyx.net.http.
-import static groovyx.net.http.ContentType.JSON
-import groovy.json.JsonSlurper
-import groovy.json.JsonOutput
 def execute(){
 
     return ["status":"OK", "details":System.currentTimeSeconds().toString(), "solution_suggestion":""]
